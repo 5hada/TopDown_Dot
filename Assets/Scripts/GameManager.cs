372f0995cce24c36caf67e7e8930a94cef41a83a
5hada <81249902+5hada@users.noreply.github.com>
2025-04-05 06:58:07 +0900

    대화 시스템
    오브젝트 관리, 대화 기능, 초상화 설정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,33 +4,45 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public GameObject talkPanel;
+    public Image profileImg;
     public TextMeshProUGUI talkText;
     public GameObject scanObject;
     public bool isAction;
+    public int talkIndex;
     public void Action(GameObject scanObj)
     {
-        if(isAction)
-        {
-            isAction = false;
-        }
-        else
-        {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은 " + scanObject.name + "이라고 한다.";
-        }
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+        
         talkPanel.SetActive(isAction);
 
     }
-    void Start()
+    void Talk(int id, bool isNpc)
     {
-        
-    }
+        string talkData = talkManager.GetTalk(id,talkIndex);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(talkData == null)
+        {
+            talkIndex = 0;
+            isAction = false;
+            return;
+        }
+        if (isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            profileImg.sprite = talkManager.GetProfile(id, int.Parse(talkData.Split(':')[1]));
+            profileImg.color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            talkText.text = talkData;
+            profileImg.color = new Color(1, 1, 1, 0);
+        }
+        isAction = true;
+        talkIndex++;
     }
 }
---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+    Dictionary<int, Sprite> profileData;
+
+    public Sprite[] profileArr;
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        profileData = new Dictionary<int, Sprite>();
+        GenerateData();
+    }
+    
+    void GenerateData()
+    {
+        talkData.Add(1000, new string[] { "안녕?:0", "이 곳에 처음 왔구나?:1" });
+        talkData.Add(2000, new string[] { "여어.:1", "이 호수는 정말 아름답지?:0", "사실 이 호수에는 무언가의 비밀이 숨겨져있다고 해.:1"});
+        talkData.Add(100, new string[] { "평범한 나무상자다." });
+        talkData.Add(400, new string[] { "누군가 사용했던 흔적이 있는 책상이다." });
+
+        profileData.Add(1000 + 0, profileArr[0]);
+        profileData.Add(1000 + 1, profileArr[1]);
+        profileData.Add(1000 + 2, profileArr[2]);
+        profileData.Add(1000 + 3, profileArr[3]);
+        profileData.Add(2000 + 0, profileArr[4]);
+        profileData.Add(2000 + 1, profileArr[5]);
+        profileData.Add(2000 + 2, profileArr[6]);
+        profileData.Add(2000 + 3, profileArr[7]);
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+
+    public Sprite GetProfile(int id, int profileIndex)
+        {
+        return profileData[id + profileIndex];
+    }
+}
